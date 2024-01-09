17f2d7e4d65c957234fdb90761766f9f9f30cc9c
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-09 14:49:43 -0600

    Add isClove(text) function

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
 from aqt import mw
 from aqt.utils import showInfo, qconnect, TextFormat
 from aqt.qt import *
+def isClove(text) -> bool:
+    """
+    Checks whether or not a card face has Clove text formatting.
+    
+    Returns: 
+        - bool
+    """
+    if ("{{" in text & "}}" in text):
+        return True
+    return False
+
